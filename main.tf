a7a3c44a2133299483357f08efa274ef3c9bd7e5
Tunahan Dursun <tunahandursun@hotmail.com>
2021-12-21 11:17:03 +0300

    ww

--- main.tf
+++ main.tf
@@ -15,7 +15,7 @@ provider "aws" {
 }
 
 resource "aws_spot_instance_request" "master_nodes" {
-  ami                    = "ami-05f7491af5eef733a"
+  ami                    = var.image_id
   spot_price             = "0.018"
   instance_type          = "t2.medium"
   spot_type              = "one-time"
